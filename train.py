1fe1aa4567aa1d6c54a955c70122ba5419091a23
Hiroshiba Kazuyuki <kazuyuki_hiroshiba@dwango.co.jp>
2021-03-30 08:17:58 +0000

    to long val melspec

--- train.py
+++ train.py
@@ -196,9 +196,11 @@ def train(rank, a, h):
                             y_g_hat_mel = mel_spectrogram(y_g_hat.squeeze(1), h.n_fft, h.num_mels, h.sampling_rate,
                                                           h.hop_size, h.win_size,
                                                           h.fmin, h.fmax_for_loss)
-                            if y_mel.shape[2] > y_g_hat_mel.shape[2]:
-                                assert y_mel.shape[2] - y_g_hat_mel.shape[2] < 5
-                                y_mel = y_mel[:, :, :y_g_hat_mel.shape[2]]
+                            if y_mel.shape[2] != y_g_hat_mel.shape[2]:
+                                assert abs(y_mel.shape[2] - y_g_hat_mel.shape[2]) < 5
+                                min_length = min(y_mel.shape[2], y_g_hat_mel.shape[2])
+                                y_mel = y_mel[:, :, :min_length]
+                                y_g_hat_mel = y_g_hat_mel[:, :, :min_length]
                             val_err_tot += F.l1_loss(y_mel, y_g_hat_mel).item()
 
                             if j <= 4:
